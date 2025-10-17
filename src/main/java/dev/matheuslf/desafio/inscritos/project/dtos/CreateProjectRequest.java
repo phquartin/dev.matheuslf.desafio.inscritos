ce81ca133ccf4f15f236342a57fea8e00ebddf5d
phquartin <phquartinmatos@sempreceub.com>
2025-10-16 22:28:31 -0300

    fix(project): ajustar validação do campo name no DTO de criação

--- src/main/java/dev/matheuslf/desafio/inscritos/project/dtos/CreateProjectRequest.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/project/dtos/CreateProjectRequest.java
@@ -2,12 +2,14 @@ package dev.matheuslf.desafio.inscritos.project.dtos;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 
 public record CreateProjectRequest(
         @NotBlank(message = "name cannot be blank")
-        @Min(value = 3, message = "name must have at least 3 characters")
+        @Length(min = 3, max = 100, message = "name must have at least 3 characters")
+        @Length(max = 100, message = "name cannot have more than 100 characters")
         String name,
         String description,
         LocalDate startDate,
