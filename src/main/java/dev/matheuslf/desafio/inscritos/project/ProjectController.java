8d684fb46f7ff5a375f26d387729b70ac682bb86
phquartin <phquartinmatos@sempreceub.com>
2025-10-16 22:04:12 -0300

    feat(project): criar ProjectController com endpoints para salvar e listar projetos

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/project/ProjectController.java
@@ -0,0 +1,30 @@
+package dev.matheuslf.desafio.inscritos.project;
+
+import dev.matheuslf.desafio.inscritos.project.dtos.CreateProjectRequest;
+import dev.matheuslf.desafio.inscritos.project.dtos.ProjectResponse;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/projects")
+public class ProjectController {
+
+    private final ProjectService service;
+    public ProjectController(ProjectService service) {
+        this.service = service;
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProjectResponse> save(@RequestBody @Valid CreateProjectRequest request){
+        ProjectResponse saved = service.save(request);
+        return ResponseEntity.status(201).body(saved);
+    }
+    @GetMapping
+    public ResponseEntity<Page<ProjectResponse>> findAll(Pageable pageable){
+        return ResponseEntity.ok(service.findAll(pageable));
+    }
+
+}
