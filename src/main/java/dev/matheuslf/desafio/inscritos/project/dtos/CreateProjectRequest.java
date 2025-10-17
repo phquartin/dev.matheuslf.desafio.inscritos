eedf326fc376a0ad9fb7e6fbef286ca519cc1950
phquartin <phquartinmatos@sempreceub.com>
2025-10-16 22:17:27 -0300

    fix(project): adicionar validação mínima para o campo name no DTO de criação

--- src/main/java/dev/matheuslf/desafio/inscritos/project/dtos/CreateProjectRequest.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/project/dtos/CreateProjectRequest.java
@@ -1,11 +1,13 @@
 package dev.matheuslf.desafio.inscritos.project.dtos;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
 
 public record CreateProjectRequest(
         @NotBlank(message = "name cannot be blank")
+        @Min(value = 3, message = "name must have at least 3 characters")
         String name,
         String description,
         LocalDate startDate,
