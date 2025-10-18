9e320d138965c961b2c1c2f1715abf7b84ce3412
phquartin <phquartinmatos@sempreceub.com>
2025-10-18 01:50:23 -0300

    refactor(project): remover importações não utilizadas em ProjectRepository

--- src/main/java/dev/matheuslf/desafio/inscritos/project/ProjectRepository.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/project/ProjectRepository.java
@@ -3,8 +3,6 @@ package dev.matheuslf.desafio.inscritos.project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectModel, Long> {
 }
